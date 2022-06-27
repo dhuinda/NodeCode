a2113309b71527aa92561f0c8353f8f0ba844165
DannyTheSloth <39781618+DannyTheSloth@users.noreply.github.com>
2022-06-27 11:51:32 -0500

    Fix config

--- CodeDesigner.UI/Properties/Resources.Designer.cs
+++ CodeDesigner.UI/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace CodeDesigner.UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Configure_64px {
+            get {
+                object obj = ResourceManager.GetObject("Configure_64px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Exit_35px_RED {
             get {
                 object obj = ResourceManager.GetObject("Exit_35px_RED", resourceCulture);
